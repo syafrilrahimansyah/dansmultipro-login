ea795a8daaa007d6f76ad442b88a3206a712516e
root <syafrilrahimansyah1@gmail.com>
2022-07-23 10:23:56 +0700

    first commit

---
+++ src/test/java/com/dansmultipro/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dansmultipro.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
